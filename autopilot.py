05e60417d05d96008e412e08f13455dbe6a47e5d
Luke Kerbs <LDK.kerbs@gmail.com>
2025-02-19 19:54:46 -0800

    updated

--- autopilot.py
+++ autopilot.py
@@ -6,38 +6,63 @@ import time
 
 pyautogui.FAILSAFE = False
 
-
 print(datetime.datetime.now().strftime("%H:%M"))
-def check_active_status(last_location):
-	return pyautogui.position() != last_location
 
-print('--👨🏽‍✈️ AUTOPILOT 👨🏽‍✈️--')
+
+def check_active_status(last_location):
+    return pyautogui.position() != last_location
+
+
+print("--👨🏽‍✈️ AUTOPILOT 👨🏽‍✈️--")
 print(f'Start: {datetime.datetime.now().strftime("%H:%M")}')
 screen_size = pyautogui.size()
 x, y = screen_size[0], screen_size[1]
 
-default_end_time = datetime.time(17,30) # 5:30
+default_end_time = datetime.time(17, 30)  # 5:30
 if datetime.datetime.now().time() < default_end_time:
-	end_time = default_end_time
+    end_time = default_end_time
 else:
-	# end one hour from now if initiated past end time
-	end_time = (datetime.datetime.now() + datetime.timedelta(hours=1)).time() 
+    # end one hour from now if initiated past end time
+    end_time = (datetime.datetime.now() + datetime.timedelta(hours=1)).time()
+
+# Initial direction
+x_direction = 1  # 1 for right, -1 for left
+y_direction = 1  # 1 for down, -1 for up
+x_pos = random.randint(0, x)
+y_pos = random.randint(0, y)
+move_speed = 50  # pixels per movement
 
 while datetime.datetime.now().time() < end_time:
-	# move to new position
-	x_destination = random.randint(0, x)
-	y_destination = random.randint(0, y)
-	pyautogui.moveTo(x_destination, y_destination, duration=1)
-	time.sleep(random.randint(1,7))
+    # Update position
+    x_pos += move_speed * x_direction
+    y_pos += move_speed * y_direction
 
-	# wait for 60 seconds if active
-	last_location = (x_destination, y_destination)
-	active = check_active_status(last_location)
-	while active:
-		last_location = pyautogui.position()
-		time.sleep(60)
-		active = check_active_status(last_location)
+    # Check for collisions with screen edges
+    if x_pos >= x:
+        x_pos = x
+        x_direction = -1
+    elif x_pos <= 0:
+        x_pos = 0
+        x_direction = 1
+
+    if y_pos >= y:
+        y_pos = y
+        y_direction = -1
+    elif y_pos <= 0:
+        y_pos = 0
+        y_direction = 1
+
+    # Move cursor
+    pyautogui.moveTo(x_pos, y_pos, duration=0.001)
+
+    # Check if user is moving mouse
+    last_location = (x_pos, y_pos)
+    active = check_active_status(last_location)
+    while active:
+        last_location = pyautogui.position()
+        time.sleep(1)
+        active = check_active_status(last_location)
+        if not active:
+            x_pos, y_pos = last_location
 
 print(f'Close: {datetime.datetime.now().strftime("%H:%M")}')
-
-
